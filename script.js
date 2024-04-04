e0efab07a2bddd8bacdeaadf6bbff173d21f9e38
ahmed sayed abdelhay <ahmed01090857607@gmail.com>
2024-04-05 01:13:51 +0200

    four commit

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ themeToggle.addEventListener("click", () => {
   sun.classList.toggle("show");
   moonn.classList.toggle("show");
 });
+
+// let lastScrollTop = 0;
+// const navbar = document.getElementById("navbar");
+// const nav = document.getElementById("nav");
+
+// window.addEventListener("scroll", function () {
+//   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+//   if (scrollTop > lastScrollTop) {
+//     // Scrolling down
+//     navbar.style.transform = "translateY(-100%)";
+//     nav.style.transform = "translate(0)";
+//   } else {
+//     // Scrolling up
+//     navbar.style.transform = "translateY(0)";
+//     nav.style.transform = "translate(100%)";
+//   }
+
+//   lastScrollTop = scrollTop;
+// });
